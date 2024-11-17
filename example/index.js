24f4f1bc3e2db9962c4498a988d23847dd6541b9
Efrem Ropelato <efrem.ropelato@gmail.com>
2024-11-17 09:23:15 +0100

    improve example

--- example/index.js
+++ example/index.js
@@ -3,23 +3,30 @@ const CrossDB = require('../index');
 const db = new CrossDB(':memory:');
 
 try {
-    console.log("Create");
     db.exec("CREATE TABLE users (id INT PRIMARY KEY, name CHAR(50), age INT)");
+    console.log("Table 'users' created.");
     db.begin();
-    console.log("Insert");
+    console.log("Begin transaction");
     db.exec("INSERT INTO users (id, name, age) VALUES (1, 'Alice', 30), (2, 'Bob', 25), (3, 'Charlie', 35)");
-    console.log("Select");
+    console.log("Data inserted.");
     let res = db.exec("SELECT * FROM users");
-    console.log("All users: ", res);
-    console.log("Delete");
-    db.exec("DELETE FROM users where id=1");
+    console.log("Select all users: ", res);
+    db.exec("UPDATE users SET age = 31 WHERE id = 2");
+    console.log("User with ID 2 updated.");
     res = db.exec("SELECT * FROM users");
-    console.log("All users: ", res);
+    console.log("Select all users: ", res);
+    db.exec("DELETE FROM users where id=1");
+    console.log("User with ID 1 deleted.");
+    res = db.exec("SELECT * FROM users");
+    console.log("Select all users: ", res);
     db.commit();
+    console.log("Commit transaction");
 } catch (err) {
-    console.error(err);
+    console.error("Error during operation:", err);
     db.rollback();
+    console.log("Rollback transaction");
 } finally {
-    console.log("Close");
+    console.log("\nCrossDB Simulation Complete.");
     db.close();
+    console.log("Database connection closed.");
 }
